3bd63ad8f72ecf1791c5ecf15805ff77e8712f9d
AleeWeb <aalexanderpr@gmail.com>
2018-10-09 01:48:45 -0400

    Urls.py and settings.py config to get the served image directly through the link within the API response.

--- user_review/settings.py
+++ user_review/settings.py
@@ -112,4 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'user_review')
